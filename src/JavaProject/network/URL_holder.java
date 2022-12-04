0a47f345fdd8a3b08182da56757773a160411f51
Oracionspace <shipfriend4371@naver.com>
2022-12-04 14:15:20 +0900

    Main.java 안에 있는 클래스를 독립 외부 클래스로 뻄

---
+++ src/JavaProject/network/URL_holder.java
@@ -0,0 +1,8 @@
+package JavaProject.network;
+
+public class URL_holder {
+    private static final String URL="http://13.124.228.32:8082/";
+    public static String getURL(){
+        return URL;
+    }
+}
