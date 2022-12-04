652b4c9a871fc3b1785088066063c1adb041d25b
JeongwooSeo <98446924+ShipFriend0516@users.noreply.github.com>
2022-12-04 14:25:19 +0900

    Delete URL_holder.java

--- src/JavaProject/network/URL_holder.java
+++
@@ -1,8 +0,0 @@
-package JavaProject.network;
-
-public class URL_holder {
-    private static final String URL="http://13.124.228.32:8082/";
-    public static String getURL(){
-        return URL;
-    }
-}
